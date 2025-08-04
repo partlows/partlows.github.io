edd2c40956837e120c53514762fed7364cac6526
partlows <partlows16@gmail.com>
2025-08-04 14:27:49 -0400

    -fix basePath for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/partlows.github.io' : '',
+  basePath: '',
 };
 
 export default nextConfig;
