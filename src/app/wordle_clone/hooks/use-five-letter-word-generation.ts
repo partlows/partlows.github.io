23cae78de3e5503294c4d8d6bf536e46aa752002
partlows <partlows16@gmail.com>
2025-08-08 00:09:25 -0400

    -new word

--- src/app/wordle_clone/hooks/use-five-letter-word-generation.ts
+++ src/app/wordle_clone/hooks/use-five-letter-word-generation.ts
@@ -1,5 +1,5 @@
 export function useFiveLetterWordGeneration() {
-  const wordToGuess = "Point"; // TODO: replace with api call
+  const wordToGuess = "Pesto"; // TODO: replace with api call
   return {
     wordToGuess: wordToGuess.toLocaleUpperCase(),
   };
