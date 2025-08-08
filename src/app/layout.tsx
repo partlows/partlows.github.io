dbdc092726fd2fd531b26dd4b48e540125072050
partlows <partlows16@gmail.com>
2025-08-08 16:03:58 -0400

    -starting to fix responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,19 +29,21 @@ export default function RootLayout({
       style={{
         fontFamily: defaultFont.style.fontFamily,
         backgroundColor: "#b1b4ef",
+        height: "100%",
       }}
     >
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
           href="https://api.fontshare.com/v2/css?f[]=satoshi@301&display=swap"
           rel="stylesheet"
         />
       </head>
-      <body style={{ margin: 0 }}>
+      <body style={{ margin: 0, padding: 0, height: "100%" }}>
         <header>
           <Header />
         </header>
-        {children}
+        <main>{children}</main>
         <footer>
           <Footer />
         </footer>
