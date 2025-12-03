771c9169119951bc22f81c5f51ca7e0a9898fce6
partlows <partlows16@gmail.com>
2025-12-03 12:49:31 -0500

    -new word

--- src/app/samdle/hooks/use-five-letter-word-generation.ts
+++ src/app/samdle/hooks/use-five-letter-word-generation.ts
@@ -1,5 +1,5 @@
 export function useFiveLetterWordGeneration() {
-  const wordToGuess = "Plumb"; // TODO: replace with api call
+  const wordToGuess = "Scope"; // TODO: replace with api call
   return {
     wordToGuess: wordToGuess.toLocaleUpperCase(),
   };
