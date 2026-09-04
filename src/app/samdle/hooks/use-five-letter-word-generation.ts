9b3d67e424ed515bddfc65acbf0b83673faef07a
Sam Partlow <31989960+partlows@users.noreply.github.com>
2026-09-04 18:53:41 -0400

    Update use-five-letter-word-generation.ts

--- src/app/samdle/hooks/use-five-letter-word-generation.ts
+++ src/app/samdle/hooks/use-five-letter-word-generation.ts
@@ -1,5 +1,5 @@
 export function useFiveLetterWordGeneration() {
-  const wordToGuess = "Inked"; // TODO: replace with api cal
+  const wordToGuess = "Toast"; // TODO: replace with api cal
   return {
     wordToGuess: wordToGuess.toLocaleUpperCase(),
   };
